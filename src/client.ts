79f164e7876ce5ee54262ee2f6d1274d86cb8c50
Maidons <matt.sneg@gmail.com>
2025-06-05 18:53:01 +0000

    Initial commit

---
+++ src/client.ts
@@ -0,0 +1,96 @@
+import { Emitter } from './emitter';
+import type { UserData, EmptyRequests, PayloadRequests, MessageFromServer } from './types';
+
+type RequestUrl = Parameters<typeof fetch>[0];
+type RequestOptions = Parameters<typeof fetch>[1];
+
+interface AppOptions {
+  customFetch?: typeof fetch;
+  autoinit?: boolean;
+}
+export default class SPWMini extends Emitter<MessageFromServer> {
+  appId: string;
+  isReady: boolean = false;
+  user: UserData | null = null;
+  customFetch: typeof fetch | null = null;
+
+  #send<T extends keyof PayloadRequests>(type: T, payload: PayloadRequests[T]): void;
+  #send<T extends keyof EmptyRequests>(type: T): void;
+  #send<T extends keyof PayloadRequests | keyof EmptyRequests>(
+    type: T,
+    payload?: PayloadRequests[T & keyof PayloadRequests]
+  ): void {
+    if (payload) return window.parent.postMessage({ type, payload }, '*');
+    return window.parent.postMessage({ type }, '*');
+  }
+
+  #handleMessage({ data }: { data: any }) {
+    if (!data || typeof data !== 'object') return;
+    if (!data.type) return;
+    this.emit(data.type, data.payload);
+  }
+
+  constructor(appId: string, options?: AppOptions) {
+    super();
+    this.appId = appId;
+
+    if (options?.customFetch)
+      this.customFetch = options.customFetch;
+
+    // autoinit can be undefined - true
+    const doAutoInit = !options || options.autoinit !== false;
+    if (doAutoInit) this.initialize();
+
+    this.once('initResponse', user => {
+      this.isReady = true;
+      this.user = user;
+      if (this.isReady) this.emit('ready');
+    });
+  }
+
+  initialize() {
+    window.addEventListener('message', this.#handleMessage.bind(this));
+    this.#send('init', this.appId);
+  }
+
+  validateUser(url: RequestUrl, init: RequestOptions = {}) {
+    return new Promise<boolean>((resolve, reject) => {
+      if (!init.method) init.method = 'POST';
+      if (!init.headers) init.headers = {};
+
+      // @ts-expect-error
+      init.headers['Content-Type'] = 'application/json';
+
+      const fetchFunction = this.customFetch || window.fetch;
+
+      fetchFunction(url, {
+        ...init,
+        body: JSON.stringify(this.user)
+      })
+        .then(r => r.text())
+        .then(v => resolve(v === '1'))
+        .catch(reject);
+    });
+  }
+
+  dispose() {
+    window.removeEventListener('message', this.#handleMessage.bind(this));
+  }
+
+  openURL(url: string) {
+    let parsedUrl;
+    try {
+      parsedUrl = new URL(url);
+    } catch (e) {
+      throw 'Невалидный URL';
+    }
+    if (parsedUrl.protocol !== 'https:') throw 'Неверный протокол';
+
+    this.#send('openURL', url);
+  }
+
+  openPayment(code: string) {
+    if (!code) throw 'Не указан код';
+    this.#send('openPayment', code);
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,52 @@
+type Requests = {
+  init: string;
+  openURL: string;
+  openPayment: string;
+};
+
+export type EmptyRequests = {
+  [K in keyof Requests as Requests[K] extends undefined ? K : never]: undefined;
+};
+export type PayloadRequests = Omit<Requests, keyof EmptyRequests>;
+
+type Responses = {
+  init: UserData;
+  openURL: 'success';
+  openPayment: 'success';
+  payment: 'success' | 'cancel';
+};
+
+type SuccessResponses = {
+  [K in keyof Responses as `${K}Response`]: Responses[K];
+};
+
+type ErrorResponses = {
+  [K in keyof Responses as `${K}Error`]: string;
+};
+
+type PlainResponses = {
+  ready: never;
+}
+
+export type MessageFromServer = SuccessResponses & ErrorResponses & PlainResponses;
+
+export interface User {
+  /** @deprecated Будет удалён через некоторое время, используй `accountId` */
+  discordId: string;
+  accountId: string;
+  username: string;
+  minecraftUUID: string;
+}
+
+export interface UserData extends User {
+  hash: string;
+}
+
+type DoMessageData<T> = {
+  [K in keyof T]: { type: K };
+}[keyof T];
+type DoMessageLoadedData<T> = {
+  [K in keyof T]: { type: K; payload: T[K] };
+}[keyof T];
+
+export type RequestMessage = DoMessageData<EmptyRequests> | DoMessageLoadedData<PayloadRequests>;
